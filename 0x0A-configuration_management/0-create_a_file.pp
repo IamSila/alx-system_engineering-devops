9d7526f5b562251b94c4dc35b9f6607fd7a4a572
IamSila <silamulingi@gmail.com>
2024-08-06 17:47:40 +0300

    puppet task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,8 @@
+#!/usr/bin/pup
+# creates a file called school in directory etc
+
 file{'/tmp/school':
-    path    => '/etc/school',
-    ensure  => 'file',
+    path    => '/tmp/school',
     content => 'I love Puppet',
     mode    => '0744',
     owner   => 'www-data',
