3c94323210a1b859f79b39d57ada415dc058e04d
IamSila <silamulingi@gmail.com>
2024-08-11 12:47:21 +0300

    ssh task 0

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,15 @@
+#!/usr/bin/pup
 # install flask
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command  => '/usr/bin/pip3 install Flask==2.1.0',
+  path     => ['/usr/bin', '/usr/local/bin'],
+  unless   => '/usr/bin/pip3 show Flask | grep "Version: 2.1.0"',
+  provider => shell,
 }
